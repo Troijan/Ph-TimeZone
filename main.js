8549e6f1140bf15e54b4cd7a97f6e48cef73e4d0
Reynard cayetano <92937542+Troijan@users.noreply.github.com>
2024-06-07 11:38:31 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const hours = document.getElementById("hours");
+const mins = document.getElementById("mins");
+const secs = document.getElementById("secs");
+
+setInterval(() => {
+  let currentTime = new Date();
+  hours.innerHTML = (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+  mins.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+  secs.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+}, 1000);
